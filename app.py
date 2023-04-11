7dbddfd4180d061cbafb8e71e4378225f8782cce
Sudeep Kumar Srivastava <sudeepsri23@gmail.com>
2023-04-11 22:31:29 +0530

    get-total function

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from preprocessing import preprocessor
+from total import get_total
 
 with st.sidebar :
     st.title("WhatsApp Chat Analyzer")
@@ -8,3 +9,14 @@ with st.sidebar :
 if file :
     data = file.getvalue().decode("utf-8")
     df = preprocessor(data)
+
+    unique_users = df["user"].unique().tolist()
+    unique_users.remove("WhatsApp Notifications")
+    unique_users.sort()
+    unique_users.insert(0, "All users")
+    with st.sidebar :
+        user = st.selectbox(label="Choose a user", options=unique_users, index=0)
+        click = st.button(label="Show", type="primary")
+
+    if click :
+        get_total(df, user)
---
+++ total.py
@@ -0,0 +1,36 @@
+import streamlit as st
+from urlextract import URLExtract
+
+def get_total (data, user) :
+
+    if user == "All users" :
+        df = data
+    else :
+        df = data[(data["user"] == user)]
+
+    messages = df["message"].tolist()
+
+    words = []
+    for i in df["message"] :
+        words.extend(i.split())
+
+    media = df[(df["message"] == "<Media omitted>\n")]
+
+    extractor = URLExtract()
+    links = []
+    for i in df["message"] :
+        links.extend(extractor.find_urls(i))
+
+    c1, c2, c3, c4 = st.columns(spec=[1, 1, 1,  1], gap="small")
+    with c1:
+        st.header("Total Messages typed")
+        st.subheader(len(messages))
+    with c2:
+        st.header("Total Words used")
+        st.subheader(len(words))
+    with c3:
+        st.header("Total Media shared")
+        st.subheader(media.size)
+    with c4:
+        st.header("Total Links shared")
+        st.subheader(len(links))
